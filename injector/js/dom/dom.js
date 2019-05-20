5b8cbd7d68977825e9d5bf791277eed7f9714766
ac2pic <ac2pic@gmail.com>
2019-05-20 00:35:03 -0500

    Added findallScripts for DOM

--- injector/js/dom/dom.js
+++ injector/js/dom/dom.js
@@ -90,6 +90,9 @@ export default class DOM {
 		return null;
 	}
 
+	findAllScripts() {
+		return Array.from(this.document.getElementsByTagName('script'));
+	}
 	toString() {
 		if (!this.document) {
 			return '';
