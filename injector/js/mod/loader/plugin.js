2bdb333b6595f91415270dbb91931654cd3abb23
ac2pic <ac2pic@gmail.com>
2019-05-20 20:19:57 -0500

    Added code to Inject custom path for plugins

--- injector/js/mod/loader/plugin.js
+++ injector/js/mod/loader/plugin.js
@@ -1,5 +1,7 @@
 import BasicLoader from './basic.js';
 import PluginModel from '../models/plugin.js';
+import Path from '../../path/path.js';
+
 export default class PluginLoader extends BasicLoader {
 
 	addGlobalPath() {
@@ -12,16 +14,30 @@ export default class PluginLoader extends BasicLoader {
 	async load() {
 		let loadedPlugins = [];
 		for (const pluginFolder of this.plugins) {
-			const pluginPath = this.path.joinWithPath({
-				pathKey : 'plugins-browser',
-				relativePath : [pluginFolder, 'plugin.js']
+			const pluginPath = new Path(this.env);
+			const pluginBrowserBase = this.path.joinWithPath({
+				pathKey: 'plugins-browser',
+				relativePath: [pluginFolder]
+			});
+			const pluginAbsoluteBase = this.path.joinWithPath({
+				pathKey: 'plugins',
+				relativePath: [pluginFolder]
+			});
+			pluginPath.setBase({
+				browser: pluginBrowserBase,
+				absolute: pluginAbsoluteBase
+			});
+			const pathToPlugin = pluginPath.joinWithPath({
+				pathKey: 'base-browser',
+				relativePath: ['plugin.js']
 			});
 			try {
-				const result = await import(pluginPath);
+				const result = await import(pathToPlugin);
 				const plugin = new PluginModel(result);
+				plugin.setPath(pluginPath);
 				loadedPlugins.push(plugin);
 			} catch (e) {
-				console.log(`Failed to load "${pluginPath}"`);
+				console.log(`Failed to load "${pathToPlugin}"`);
 			}
 		}
 		return loadedPlugins;
