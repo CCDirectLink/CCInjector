c1e349e58220770876042eb44ebf2f7aacf3709f
ac2pic <ac2pic@gmail.com>
2019-05-21 19:39:24 -0500

    ModModel no longer injects it's code into the browser.

--- injector/js/mod/models/mod.js
+++ injector/js/mod/models/mod.js
@@ -7,19 +7,4 @@ export default class ModModel {
 		this.main = modPackage.main;
 		this.path = path;
 	}
-	execute() {
-		return new Promise((resolve, reject) => {
-			console.log(`Silly dom addition of  ${this.name}.`);
-			const script = document.createElement('script');
-			script.type = 'module';
-			script.onload = () => {
-				console.log(`${this.name} executed successfully`);
-				resolve();
-			};
-			script.src = this.path + '/' + this.main;
-			script.id = this.name;
-			document.body.appendChild(script);	
-		});
-		
-	}
 }
