90798cba7d0412e538d73d0970df320564afa018
ac2pic <ac2pic@gmail.com>
2019-05-27 08:13:41 -0500

    Moved priority logic from PluginManager to BasicManager

--- injector/js/mod/manager/basic.js
+++ injector/js/mod/manager/basic.js
@@ -1,24 +1,74 @@
 import Path from '../../path/path.js';
 import BasicLoader from '../loader/basic.js';
 import BasicModel from '../models/basic.js';
-export default class BasicManager {
-	constructor(path, env, fs, resLoader, Loader = BasicLoader, Model = BasicModel) {
+import PriorityManager from './priority.js';
+
+export default class BasicManager extends PriorityManager {
+	constructor(path, env, fs, resLoader, models, Loader = BasicLoader, Model = BasicModel) {
+		super();
 		this.path = path;
 		this.env = env;
 		this.fs = fs;
 		this.resLoader = resLoader;
 
 		this.loader = new Loader(path, env, fs, resLoader);
-		this.model = Model;
-		this.models = [];
+		this.Model = Model;
+		this.models = models;
 		this.type = 'basic';
+		this.cacheModels = null;
 	}
 	setType(type) {
 		this.type = type;
 	}
 	
+	setCacheModels(models) {
+		this.cacheModels = models;
+	}
+	
 	setModels(models) {
-		this.models = models;
+		
+		const [lowestPriority, highestPriority] = this.getPriorityRange(models);
+		if (models.length > 0) {
+			if(!this._isPriorityAtOrAboveMinPriority(lowestPriority)) {
+				throw new RangeError(`The lowest model priority ${lowestPriority} is below ${this.getMinPriority()}.`);
+			}		
+		} 
+
+		this.setMinPriority(lowestPriority);
+		this.setMaxPriority(highestPriority);
+		this.setCacheModels(models);
+		
+		const diffIndex = this._getDifferenceInModelLengths();
+		this.models.splice.apply(this.models, [diffIndex, models.length].concat(models));
+	
+	}
+
+	_isPriorityAtOrAboveMinPriority(priority) {
+		return priority >= this.getMinPriority(); 
+	}
+
+	_isModelAddable(model) {
+		return model.getPriority() >= this.getMinPriority();
+	}
+	
+	getPriorityRange(models) {
+		if (models.length === 0) {
+			return [-Infinity, Infinity];
+		}
+		const lastElementIndex = models.length - 1;
+		let lowestPriority = models[0].getPriority();
+		let highestPriority = models[lastElementIndex].getPriority();
+		
+		for(let i = 0; i < models.length; i++) {
+			const modelPriority = models[i].getPriority();
+			if (modelPriority < lowestPriority) {
+				lowestPriority = modelPriority;
+			}
+			if (modelPriority > highestPriority) {
+				highestPriority = modelPriority;;
+			}
+		}
+		return [lowestPriority, highestPriority];
 	}
 	
 	async init() {
@@ -30,9 +80,13 @@ export default class BasicManager {
 	async run() {}
 
 	createModel(modelData) {
-		return new this.model(modelData);
+		return new this.Model(modelData);
 	}
+	
 	addModel(model) {
+		if (!this._isModelAddable(model)) {
+			throw new RangeError(`Can't add "${model.getName()}" with lower priority than ${this.getMinPriority()}`);
+		}
 		this.models.push(model);
 	}
 
@@ -40,11 +94,27 @@ export default class BasicManager {
 		return this.getModels().indexOf(model);
 	}
 	
+	getIndexOfModelWithPriority(priority) {
+		const models = this.getModels();
+		for(let i = 0; i < models.length; i++) {
+			if (models[i].getPriority() === priority) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	insertModel(model, index) {
+		if (!this._isModelAddable(model)) {
+			throw new RangeError(`Can't insert "${model.getName()}" with lower priority than ${this.getMinPriority()}`);
+		}
 		this.models.splice(index, 0, model);
 	}
 	
 	replaceModel(model, index) {
+		if (!this._isModelAddable(model)) {
+			throw new RangeError(`Can't override ${this.models[index].getName()} with "${model.getName()}" since it has a lower priority than ${this.getMinPriority()}`);
+		}
 		this.models.splice(index, 1, model);
 	}
 	
@@ -78,8 +148,50 @@ export default class BasicManager {
 	}
 
 	getModels() {
-		return this.models;
+		if (this.getMinPriority() === -Infinity && 
+		    this.getMaxPriority() === Infinity) {
+			return this.models;
+		}
+		return this.cacheModels;
 	}
+
+	sortModelsByPriority() {
+		if (this.getModels().length === 0) {
+			return;
+		}
+
+		// sort by priority
+		let sortedModels = this.getModels().sort((model1, model2) => {
+			return model1.getPriority() - model2.getPriority();
+		});
+		// filter all models that are below the min priority
+		sortedModels = sortedModels.filter((model) => model.getPriority() >= this.getMinPriority());
+
+		this.setModels(sortedModels);
+	}
+	
+	groupModelsByPriority() {
+		if (this.models.length === 0) {
+			return [];
+		}
+		let minPriority = this.models[0].getPriority(); 
+
+		let maxPriority = this.models[this.models.length - 1].getPriority();
+		
+		const priorityList = [];
+		for(let priority = minPriority; priority <= maxPriority; priority++) {
+			const list = this.getModelsWithEqualPriority(priority);
+			priorityList.push({
+				group: list,
+				startIndex: this.getModelIndex(list[0])
+			});
+		}
+		return priorityList;
+	}
+
+	getModelsWithEqualPriority(priority) {
+		return this.getModels().filter((plugin) => plugin.getPriority() === priority);
+	}	
 	
 	_createPath(folderName) {
 		const path = new Path(this.env);
@@ -99,6 +211,37 @@ export default class BasicManager {
 	}
 	
 	copy() {
-		return new this.constructor(this.path, this.env, this.fs, this.resLoader);
+		return new this.constructor(this.path, this.env, this.fs, this.resLoader, this.models);
+	}
+
+	generateManagerWithMinPriority(priority) {
+		
+		const manager = this.copy();
+		const startIndex = this.getIndexOfModelWithPriority(priority);
+		const models = this.models.slice(startIndex);
+
+		manager.setModels(models);
+		return manager;
+	}
+
+	_getDifferenceInModelLengths() {
+		return this.models.length - this.getModels().length;
+	}
+	_getModelsIterator() {
+		let index = 0;
+		const next = () => {
+			
+			if (index < this.models.length) {
+				const currentModel = this.models[index];
+				++index;
+				return {value: currentModel, done : false};
+			}
+			return {done: true};
+		};
+		return {
+			[Symbol.iterator]: () => {
+				return {next};
+			}
+		};
 	}
 }
