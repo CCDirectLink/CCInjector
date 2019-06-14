9637e1b43a7d4cadbba96f9cf446a7cbad7ffc9b
ac2pic <ac2pic@gmail.com>
2019-06-13 23:01:06 -0500

    Hijacked dom.ready module.

--- injector/js/onload-hijacker.js
+++ injector/js/onload-hijacker.js
@@ -1,10 +1,46 @@
-window.onloadCallbacks = [];
-Object.defineProperty(window, 'onload', {
-	set(value) {
-		window.onloadCallbacks.push(value);
-		this.value = () => {};
-	},
-	get() {
-		return this.value;
-	}
-});
+
+(function () {
+	const beforeDomReady = [];
+	window.addBeforeDomReady = (cb) => {
+		beforeDomReady.push(cb);
+	};
+
+	const onloadCallbacks = [];
+	let onload = () => {},ig = {};
+	let oldDOMReady = undefined;
+	Object.defineProperties(window, {
+		onload: {
+			set(value) {;
+				onloadCallbacks.push(value);
+			},
+			get() {
+				return () => {};
+			}		
+		},
+		ig: {
+			set(value) {
+				if (window.ig !== value) {
+					oldDOMReady = value._DOMReady.bind(value);
+					let started = false;
+					value._DOMReady = ((ready = false) => {
+						if (ready === true && !started && !ig.modules["dom.ready"].loaded) {
+							started = true;
+							debugger;
+							// assume they are all promises
+							Promise.all(beforeDomReady).then(() => {
+								oldDOMReady();
+							});
+						}
+						
+					});					
+				}
+
+				ig = value;
+			},
+			get() {
+				return ig;
+			}
+		}
+
+	});	
+})()
