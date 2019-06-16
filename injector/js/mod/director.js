e467da7ee6ec1afbb4399ed0d38c7295f8f178b8
ac2pic <ac2pic@gmail.com>
2019-06-15 21:30:49 -0500

    Now handles phases smoothly

--- injector/js/mod/director.js
+++ injector/js/mod/director.js
@@ -3,14 +3,20 @@ import ResourceLoader from '../resource/loader.js';
 import Environment from '../environment/environment.js';
 
 import Logger from '../logger/log.js';
+
 import PluginManager from './manager/plugin.js';
 import ModManager from './manager/mod.js';
+
+
+import PhaseManager from './manager/phase.js';
 
 class ModDirector {
 	constructor() {
 		this.injectDependencies();
+		this.initPhases();
 	}
 	injectDependencies() {
+		this.phaseManager = new PhaseManager();
 		this.env = new Environment();
 		this.path = new Path(this.env);
 		this.resLoader = new ResourceLoader(this.path, this.env);
@@ -20,8 +26,66 @@ class ModDirector {
 		this.logger = new Logger(this.fs);
 	}
 	
+	initPhases() {
+		this.phaseManager.addPhase('preload');
+		this.phaseManager.setPhaseFilter('preload', function(model) {
+			return model.hasPriority('preload');
+		});
+
+		this.phaseManager.addPhase('postload');
+		this.phaseManager.setPhaseFilter('postload', function(model) {
+			return model.hasPriority('postload');
+		});
+		
+		this.phaseManager.addPhase('prestart');
+		this.phaseManager.addPhase('main');
+	}
+
+	initInjections() {
+		// This loads just before dom.ready
+		ig.module("mods.event.postload").defines(() => {
+			addBeforeDomReady(async function () {
+				await modDirector.triggerPhase('postload');
+			});
+
+			addBeforeDomReady(() => {
+				// this loads just before ig.main
+				ig.module("mods.event.prestart").requires("impact.base.impact").defines(() => {
+					var oldMain = ig.main;
+					ig.main = function() {
+						const args = arguments;
+						modDirector.triggerPhase('prestart').then(() => {
+							oldMain.apply(this, args);
+						});
+					};
+					this.onloadFinish();
+					
+				});
+			});
+		});
+
+
+	}
+	
+	setCurrentPhase(name) {
+		this.phaseManager.setCurrentPhase(name);
+	}
+	
+	async triggerPhase(name) {
+		this.setCurrentPhase(name);
+		await this.run();
+	}
+	
 	_managerFactory(ManagerClass) {
-		return new ManagerClass(this.path, this.env, this.fs, this.resLoader);
+		const instance = new ManagerClass();
+		instance.setPath(this.path);
+		instance.setEnv(this.env);
+		instance.setFs(require('fs'));
+		instance.setResourceLoader(this.resLoader);
+		instance.setPhaseManager(this.phaseManager);
+		instance.initLoader();
+
+		return instance;
 	}
 
 	async init() {
@@ -38,14 +102,24 @@ class ModDirector {
 		await this.modManager.run();
 	}
 	onloadFinish() {		
-		window.onloadCallbacks.forEach((cb) => cb());
+		window.executeOnloadCallbacks();
 	}
 }
 
-(async () => {
+(async function() {
+
 	const modDirector = new ModDirector();
+	window.modDirector = modDirector;
+			
 	await modDirector.init();
 	await modDirector.load();
-	await modDirector.run();
-	modDirector.onloadFinish();
+
+
+	await modDirector.triggerPhase('preload');
+	// load game.compiled.js here
+	await loadGameFile();
+	console.log('Done loading game file?');
+	modDirector.initInjections();
+	ig._DOMReady(true);
 })()
+
