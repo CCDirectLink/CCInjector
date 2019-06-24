656aeaefe37ff7b35b849e46dd011a4a25710fae
ac2pic <ac2pic@gmail.com>
2019-06-24 18:39:13 -0500

    Removed all plugins

--- assets/plugins/mod-validator/type/dependency/exporter.js
+++
@@ -1,5 +0,0 @@
-import NotFound from './not-found-model.js';
-import Found from './found-model.js';
-
-export const NotFoundDependency = NotFound;
-export const ModelDependency = Found;
--- assets/plugins/mod-validator/type/dependency/found-model.js
+++
@@ -1,7 +0,0 @@
-import BasicDependency from './basic.js';
-
-export default class ModelDependency extends BasicDependency {
-	constructor(model, index = 0) {
-		super(model, index, true, false, true);
-	}
-}
