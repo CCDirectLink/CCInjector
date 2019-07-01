fe8abefaf8e9c95323ed7f0461b921928aec0f1e
ac2pic <ac2pic@gmail.com>
2019-07-01 14:50:53 -0500

    Modified PluginModel method createInstance to return generated instance.

--- injector/js/mod/models/plugin.js
+++ injector/js/mod/models/plugin.js
@@ -1,26 +1,42 @@
 import PriorityModel from './priority.js';
+import MockPlugin from './mock-plugin.js';
+
 
 export default class PluginModel extends PriorityModel {
 	constructor(pluginModule) {
 		super(pluginModule, 'plugin');
 		this.instance = null;
-		this.createInstance(pluginModule.Class);
+		this.Class = pluginModule.Class;
 	}
 
-	// override because it should always run
-	
-	createInstance(Class) {
-		if (Class) {
-			this.instance = new Class();
-		} else {
+	/**
+	 * 
+	 * @param {Object} dependencies
+	 * @returns {MockPlugin} - Each field isn't necessarily returned 
+	 */
+	createInstance(dependencies) {
+		
+		if (!this.Class) {
 			console.log(`${this.getProperty('name')} does not have a class associated with it.`);
+		} else if (this.instance === null) {
+			this.instance = new this.Class(dependencies);
 		}
+
+		return this.instance;
 	}
 
+	/**
+	 * 
+	 * @returns {MockPlugin} - Each field isn't necessarily returned
+	 */
 	getInstance() {
 		return this.instance;
 	}
 	
+	/**
+	 * 
+	 * @returns {true}
+	 */
 	hasPriority() {
 		return true;
 	}
